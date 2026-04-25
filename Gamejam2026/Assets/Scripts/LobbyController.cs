237b4af2f18e2e74c0bce69a0771d111d1fdd6c6
Shatakshi Shrivastava <64678679+saradesu7@users.noreply.github.com>
2026-04-25 20:39:28 +0530

    lobby implemented

---
+++ Gamejam2026/Assets/Scripts/LobbyController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] panels;
+    [SerializeField] private float revealDelay = 0.75f;
+    
+    private static List<int> panelOrder = new () { 1, 5, 4, 6, 11, 2, 7, 8, 9, 10, 3, 12 };
+    
+    void Start()
+    {
+        for (int i = 0; i < panels.Length; i++)
+        {
+            int index = i;
+            Button btn = panels[i].GetComponent<Button>();
+            if (btn != null)
+                btn.onClick.AddListener(() => OnPanelClicked(index));
+        }
+
+        int count = 5; //MainController.Instance.CompletionIndex + 1;
+        StartCoroutine(RevealPanels(count));
+    }
+
+    private void OnPanelClicked(int index)
+    {
+        // do index+1
+        Debug.Log($"Panel clicked: index {index+1}");
+        MainController.Instance.SetConfigIndex(index+1);
+    }
+    
+    private IEnumerator RevealPanels(int count)
+    {
+        for (int i = 0; i < count && i < panelOrder.Count; i++)
+        {
+            int panelIndex = panelOrder[i] - 1;
+            if (panelIndex >= 0 && panelIndex < panels.Length)
+            {
+                GameObject panel = panels[panelIndex];
+                panel.SetActive(true);
+                panel.GetComponent<Image>().DOFade(1, 1).SetEase(Ease.OutCubic);
+
+                if (i == count - 1)
+                {
+                    Button btn = panel.GetComponent<Button>();
+                    if (btn != null)
+                        btn.interactable = true;
+                }
+            }
+            yield return new WaitForSeconds(revealDelay);
+        }
+    }
+}
--- Gamejam2026/Assets/Scripts/MainController.cs
+++ Gamejam2026/Assets/Scripts/MainController.cs
@@ -3,10 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class MainController : MonoBehaviour
 {
+    public static MainController Instance { get; private set; }
+
     [SerializeField] private string[] variantConfigFiles;
 
     private int selectedConfigIndex = 0;
     private bool hasLoadedGame = false;
+
+    public int CompletionIndex { get; set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
     private void OnEnable()
     {
